c2978bd909f82c1cdf0abfc23c32b4ac1bb88975
kmng1 <kmng@alumni.nus.edu.sg>
2016-08-13 03:57:51 +0000

    solution

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,7 +10,54 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = "" 
+    @wrong_guesses = "" 
+#    byebug
+
   end
+
+  attr_accessor :word, :guesses, :wrong_guesses
+  
+    #`guess`, which processes a guess and modifies the instance variables 
+  # `wrong_guesses` and `guesses` accordingly;
+  def guess(letters)
+    raise ArgumentError if letters == ""
+    raise ArgumentError if letters !~ %r{\w}
+    letters.downcase!
+      if  @word.include?(letters) 
+        !@guesses.include?(letters) ? @guesses << letters : false
+
+      else
+        @wrong_guesses.include?(letters) ? false : wrong_guesses << letters 
+      end
+  end 
+  
+  # `check_win_or_lose`, which returns one of the symbols
+  # `:win`, `:lose`, or `:play` depending on the current game state
+  def check_win_or_lose
+ 
+    if ( @wrong_guesses.length ==  7 ) 
+     :lose
+    elsif ( ! self.word_with_guesses.include?("-") )
+       :win
+    else
+      :play
+    end
+
+  end 
+  
+  # `word_with_guesses`, substitute correct guesses into the word.
+  #
+  #  INSANE ...  
+  #
+  def word_with_guesses
+    guesses_list = @guesses.to_s
+    guesses_rx = /[^ #{guesses_list}]/
+    @masked = @word.gsub(guesses_rx, "-")
+    @masked.scan(/[a-z\-]/).join
+  end
+  
+  # Get a word from remote "random word" service
 
   def self.get_random_word
     require 'uri'
